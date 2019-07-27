735aa1161938c60786be3b00ccf0bca95aa0fd63
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-27 15:02:20 -0500

    remainder

---
+++ src/Remainder.java
@@ -0,0 +1,11 @@
+public class Remainder {
+    public static void main(String[] args) {
+        double a = Math.random();
+        System.out.println(a);
+        double b = Math.random();
+        System.out.println(b);
+        double remainder = a%b;
+        System.out.print(remainder);
+    }
+}
+
