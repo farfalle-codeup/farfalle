73fbaa733dc9a8f7d4c6e6f02a3572041308947a
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-08-09 15:13:45 -0500

    modified program

--- src/Alicia/Program.java
+++ src/Alicia/Program.java
@@ -1,12 +1,10 @@
 package Alicia;
-import java.util.Arrays;
 
-//Create a function that takes an array of non-negative numbers and strings and return a new array without the strings.
 public class Program {
-    public static String[] filterArray(String[] str) {
-        return Arrays.stream(str)
-                .distinct()
-                .filter(i->i.matches("-?\\d+"))
-                .toArray(String[]::new);
+    public static int missingNum(int[] nums) {
+        int total = 55;
+        for (int i = 0; i < nums.length; i++)
+            total -= nums[i];
+        return total;
     }
 }
